49545eb331758ef39e6f60d0e56bef4ea7563af1
Niklas Long and Trevor Fast <trevor@>
2017-03-20 10:44:14 -0700

    add README, script link in html, js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var mult = function(num1, num2) {
+	return num1 * num2;
+};
+
+var num1 = parseInt(prompt("Pick a number"));
+var num2 = parseInt(prompt("Pick another number"));
+
+alert(mult(num1, num2));
