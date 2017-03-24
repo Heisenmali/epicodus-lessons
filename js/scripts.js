36c6de7b5004ec618c93120c5e717e0716668e2f
nl <niklas@long.ch>
2017-03-24 09:33:58 -0700

    add UI logic, functional js branchingand output in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,27 @@
 $(document).ready(function() {
   $("form#langpick").submit(function (event) {
-    var name = $("#name").val();
-    console.log(name);
+    event.preventDefault();
 
+    var name = $("#name").val();
+    var age = parseInt($("#age").val());
+    var company = $("input:radio[name=company]:checked").val();
+    var worktype = $("input:radio[name=worktype]:checked").val();
+    var specialisation = $("input:radio[name=specialisation]:checked").val();
+    var result;
+
+    if ((company === "startup") && (worktype === "webapp") && (specialisation === "generalist")) {
+      result = "Ruby";
+    } else if ((company === "agency") && (worktype === "android") && (specialisation === "generalist")) {
+      result = "Java";
+    } else if ((company === "agency") && (worktype === "heavyweb") && (specialisation === "specialist")) {
+      result = "PHP";
+    } else if ((worktype !== "heavyweb") && (specialisation === "specialist")) {
+      result = "Design/css";
+    } else {
+      result = "It's ok if your confused, you can choose later!"
+    }
+
+    $("div#result").text(result);
   });
 
-
-
-
-};)
+});
