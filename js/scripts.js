f7ff8a63ee716d7d944ffde1d482a13e3012056d
nl <niklas@long.ch>
2017-03-24 15:28:53 -0700

    add scroll to bottom of page, modify  styles.css for padding and results section

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $("form#langpick").submit(function (event) {
     event.preventDefault();
 
+
     var name = $("#name").val();
     var age = parseInt($("#age").val());
     var company = $("input:radio[name=company]:checked").val();
@@ -23,6 +24,11 @@ $(document).ready(function() {
 
     $("span#result").text(result);
     $("p").show();
-  });
 
+    // DISCLAIMER : The following two lines of code were added following my reading of numerous articles discussing how to use jQuery to scroll to the bottom of the page. It wasn't written by me. I left it in, as it suits the project very well and improves the UX. I have a basic understanding of how it works, but wouldn't be able to reproduce it, or come up with it by myself at this point. This is the most relevant article : https://www.electrictoolbox.com/jquery-scroll-bottom/
+
+    $('html, body').animate({scrollTop:$(document).height()}, 500);
+        return false;
+
+  });
 });
