cf67be191b3938fc82ec19c1b20413a2f005ea43
Niklas Long and Tyler Stephenson <niklas@>
2017-03-29 10:38:12 -0700

    add UI logic for input and output; base structure of html; business logic which passes first spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
+var pigLatin = function(sentence) {
+  return sentence;
+};
+
+
+
+
 $(function(){
+  $("#piglatin").submit(function(e) {
+    e.preventDefault();
+
+    var sentence = $("#userinput").val();
+    var result = pigLatin(sentence);
+
+    $("#result").text(result);
+  });
 
 
-  
 });
