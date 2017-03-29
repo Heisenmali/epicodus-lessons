22fc33c114eab84f6e4f483955696ed8df6cd78c
Niklas Long and Tyler Stephenson <tyler@>
2017-03-29 14:26:31 -0700

    add business logic to pass spec 7

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,41 @@
 var regexCheck = function(sentence) {
+
   if (sentence.match(/[a-z]/ig)) {
     return true;
+
   } else {
     return false;
   }
+
 };
 
 var consonantCheck = function(sentence) {
+
   var consonants = /[^aeiouy]/gi;
 
-  if (sentence[0].match(consonants)) {
-    return true;
+  for (i = 0; i < sentence.length; i++) {
+    if (sentence[i].match(consonants)) {
+      // continue looping
+
+    } else {
+      conCut = i;
+      return i;
+    }
   }
+
 };
 
 
 var pigLatin = function(sentence) {
 
   // var splitSentence = sentence.split("");
+  // var conCut;
+
 
   if (regexCheck(sentence) === true) {
 
-    if (consonantCheck(sentence) === true) {
-      return sentence.substr(1) + sentence.substr(0, 1) + "ay";
+    if (consonantCheck(sentence) > 0) {
+      return sentence.substr(conCut) + sentence.substr(0, conCut) + "ay";
 
     } else {
       return sentence + "ay";
@@ -41,11 +54,11 @@ $(function(){
   $("#piglatin").submit(function(e) {
     e.preventDefault();
 
+    var conCut;
+
     var sentence = $("#userinput").val();
     var result = pigLatin(sentence);
 
     $("#result").text(result);
   });
-
-
 });
