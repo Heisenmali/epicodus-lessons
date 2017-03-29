ce69d28f1ceef81369e30bab3c12e49af431b01c
Niklas Long and Tyler Stephenson <niklas@>
2017-03-29 09:24:30 -0700

    add user logic in js

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var leapYear = function(year) {
+  return false;
+}
+
+$(document).ready(function(){
+  $('#leap-year').submit(function(event){
+    event.preventDefault();
+
+    var year = parseInt($("#year").val());
+    var result = leapYear(year);
+
+    $("#result").text(result);
+  });
+});
