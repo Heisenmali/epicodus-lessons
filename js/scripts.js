a8ca22c29e937605b14cae22a1b8437c560987c7
nl <niklas@long.ch>
2017-03-31 09:56:12 -0700

    add business logic to pass all specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,59 @@
 // –––––––– Business Logic
+var divByThree = function(workingArray) {
+  workingArray.forEach(function(num) {
 
-var upCount = function(endNum) {
+    var index = workingArray.indexOf(num);
 
-  var workingArray = [];
-
-  for (let i = 1; i <= endNum; i++) {
-    workingArray.push(i);
-    console.log("result: " , workingArray);
-    console.log("i" , i);
-
-  }
+    if (num % 3 === 0 && num % 15 !== 0) {
+      workingArray[index] = "ping";
+    }
+  });
   return workingArray;
 };
 
 
+var divByFive = function(workingArray) {
 
+  workingArray.forEach(function(num) {
+    var index = workingArray.indexOf(num);
+
+    if (num % 5 === 0 && num % 15 !== 0) {
+      workingArray[index] = "pong";
+    }
+  });
+  return workingArray;
+};
+
+
+var divByFifteen = function(workingArray) {
+
+  workingArray.forEach(function(num) {
+    var index = workingArray.indexOf(num);
+
+    if (num % 15 === 0) {
+      workingArray[index] = "ping-pong";
+    }
+  });
+  return workingArray;
+};
+
+
+var upCount = function(endNum) {
+  var workingArray = [];
+
+  for (let i = 1; i <= endNum; i++) {
+    workingArray.push(i);
+
+    // console.log("result: " , workingArray);
+    // console.log("i" , i);
+
+    workingArray = divByThree(workingArray);
+    workingArray = divByFive(workingArray);
+    workingArray = divByFifteen(workingArray);
+
+  }
+  return workingArray;
+};
 
 
 
