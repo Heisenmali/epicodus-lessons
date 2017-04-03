72991b71526000d348c2625e20fecebcce4923a2
Galin McMahon and Nick Powell <galin@>
2017-04-03 11:43:47 -0700

    add ability to click back and forth from contacts.

--- js/scripts.js
+++ js/scripts.js
@@ -13,46 +13,35 @@ Contact.prototype.fullName = function() {
 
 
 $(document).ready(function() {
-    $("form#new-contact").submit(function(event) {
-      event.preventDefault();
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
 
-      var inputtedFirstName = $("input#new-first-name").val();
-      var inputtedLastName = $("input#new-last-name").val();
-      var inputtedaddress = $("input#address").val();
-      var inputtedcity = $("input#city").val();
-      var inputtedstate = $("input#state").val();
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedaddress = $("input#address").val();
+    var inputtedcity = $("input#city").val();
+    var inputtedstate = $("input#state").val();
 
-      var newContact = new Contact(inputtedFirstName,inputtedLastName,inputtedaddress,inputtedcity, inputtedstate);
+    var newContact = new Contact(inputtedFirstName,inputtedLastName,inputtedaddress,inputtedcity, inputtedstate);
 
 
 
-      $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-      // $("ul#contacts").append("<li><span class='contact'>" +  newContact.address + "</span></li>");
-      // $("ul#contacts").append("<li><span class='contact'>" +  newContact.city + "</span></li>");
-      // $("ul#contacts").append("<li><span class='contact'>" +  newContact.state + "</span></li>");
-      $("#contacts").last().click(function() {
-        $("#show-contact").show();
-        $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
-        $(".first-name").text(newContact.firstName);
-        $(".last-name").text(newContact.lastName);
-        $(".address").text(newContact.address);
-        $(".city").text(newContact.city);
-        $(".state").text(newContact.state);
+    $("ul.contact").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    // $("ul#contacts").append("<li><span class='contact'>" +  newContact.address + "</span></li>");
+    // $("ul#contacts").append("<li><span class='contact'>" +  newContact.city + "</span></li>");
+    // $("ul#contacts").append("<li><span class='contact'>" +  newContact.state + "</span></li>");
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $(".address").text(newContact.address);
+      $(".city").text(newContact.city);
+      $(".state").text(newContact.state);
 
     });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
-    });
-
-
-
-
-
-
-
-
-
-
-
+  });
 });
