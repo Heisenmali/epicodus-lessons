0568ce15614083639f55ffdff3dd941bd25508f4
Niklas Long and Nick Powell <niklas@>
2017-04-04 15:34:54 -0700

    add age and price functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,100 @@ function Viewer(movieName, age, timeOfDay) {
   this.timeOfDay = timeOfDay;
 }
 
-function Movie(movieName, ageRating, time) {
-  this.movieName = movieName;
-  this.ageRating = ageRating;
-  this.time = time;
+// function Movie(movieName, ageRating, time) {
+//   this.movieName = movieName;
+//   this.ageRating = ageRating;
+//   this.time = time;
+// }
+// function MovieTheater() {
+//   this.movies = {
+//     theDouble: {
+//       movieName: "The Double",
+//       ageRating: 18,
+//       time: [2, 4],
+//       price: 10
+//     },
+//
+//     waterWorld: {
+//       movieName: "Waterworld",
+//       ageRating: 15,
+//       time: [1, 4],
+//       price: 12
+//     },
+//
+//     avatar2: {
+//       movieName: "Avatar 2",
+//       ageRating: 16,
+//       time: [1, 3],
+//       price: 20
+//     },
+//
+//     avatar3: {
+//       movieName: "Avatar 3",
+//       ageRating: 12,
+//       time: [1, 2, 3],
+//       price: 3
+//     }
+//   }
+// }
+
+var movies = {
+  theDouble: {
+    movieName: "The Double",
+    ageRating: 18,
+    time: [2, 4],
+    price: 10
+  },
+
+  waterWorld: {
+    movieName: "Waterworld",
+    ageRating: 15,
+    time: [1, 4],
+    price: 12
+  },
+
+  avatar2: {
+    movieName: "Avatar 2",
+    ageRating: 16,
+    time: [1, 3],
+    price: 20
+  },
+
+  avatar3: {
+    movieName: "Avatar 3",
+    ageRating: 12,
+    time: [1, 2, 3],
+    price: 3
+  }
 }
 
-var theDouble = new Movie("The Double", 18, [2, 4]);
-var waterWorld = new Movie("Waterworld", 15, [1, 4]);
-var Avatar2 = new Movie("Avatar 2", 16, [1, 3]);
-var Avatar3 = new Movie("Avatar 3", 12, [1, 2, 3]);
+var agePrice = function(inputtedMovieName, newViewer, MovieTheater) {
+
+  var newPrice = movies[inputtedMovieName].price;
+  console.log(movies[inputtedMovieName].price);
+
+  if (newViewer.age <= 26) {
+    newPrice -= 1;
+  } else if (newViewer.age > 26 && newViewer.age < 70) {
+    newPrice += 1;
+  } else {
+    newPrice -= 2;
+  }
+  console.log(newPrice);
+  return newPrice;
+}
+
+
+
+var ageCheck = function(inputtedMovieName, newViewer) {
+
+
+  if (movies[inputtedMovieName]["ageRating"] <= newViewer.age) {
+    return "Please pay $" + agePrice(inputtedMovieName, newViewer, movies) + " and enjoy your overpriced movie, don't forget to pay a fortune for popcorn!";
+  } else {
+    return "Sorry you are too young to see this movie..."
+  }
+}
 
 
 // UI logic
@@ -25,15 +109,12 @@ $(function() {
     e.preventDefault();
 
     var inputtedMovieName = $("select#movie-name").val();
-    console.log(inputtedMovieName);
     var inputtedAge = parseInt($("#viewer-age").val());
-    console.log(inputtedAge);
     var inputtedTime = $("select#movie-time").val();
-    console.log(inputtedTime);
 
     var newViewer = new Viewer(inputtedMovieName, inputtedAge, inputtedTime);
-    console.log(newViewer);
 
+    $("#output #para").text(ageCheck(inputtedMovieName, newViewer));
 
   });
 
