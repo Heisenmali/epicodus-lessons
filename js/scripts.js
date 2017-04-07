d3683e73282a55c536b8d38ae5296355d8762e22
Niklas <niklas@Mbps-MacBook-Pro.local>
2017-04-07 08:49:18 -0700

    add pizza object which stores input from user and returns them correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,30 @@
+function Pizza(size, topping) {
+  this.size = size;
+  this.topping = topping;
+}
+
+// Pizza.prototype.price = function() {
+//   console.log(this.topping);
+// };
+
+
+
 $(function() {
 
-  ("#pizza-info").submit(function(e) {
+
+  $("#pizza-info").submit(function(e) {
     e.preventDefault();
 
-    
+    var size = $("#pizza-size").val();
+    var topping = $("#pizza-topping").val();
+
+    console.log(size);
+    console.log(topping);
+
+    var pizza = new Pizza(size, topping)
+
+    console.log(pizza);
+
+
   });
 });
