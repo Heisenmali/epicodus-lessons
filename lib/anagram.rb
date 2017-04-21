0a12f439e7e1a8e0f064c1a7ede57f88713f14e9
nkls <niklas@long.ch>
2017-04-21 10:03:55 -0700

    pass spec 3

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,16 @@
 # ruby code
 class String
   def anagram
-    input_array = self.downcase.split("").reject!{|char| char == " "}
+   
+    first_w_array = self.downcase.split("").reject!{|char| char == " "}
+    # second_w_array = second_word.downcase.split("").reject!{|char| char == " "}
+
+    first_char_count = Hash.new 0
+    first_w_array.each do |character|
+      first_char_count[character] += 1
+
+    
+    end
+    first_char_count 
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,8 +4,8 @@ require 'anagram'
 
 describe 'String#anagram' do
   # first spec
-  it 'takes user input and downcases, stores it in an array and removes whitespace' do
-    expect('HELlo WorlD how are you'.anagram).to(eq ["h", "e", "l", "l", "o", "w", "o", "r", "l", "d", "h", "o", "w", "a", "r", "e", "y", "o", "u"])
+  it 'takes user input and downcases, stores it in an array, removes whitespace and counts the number of characters and stores that in a hash' do
+    expect('HELlo WorlD how are you'.anagram).to(eq({'h'=>2, 'e'=>2, 'l'=>3, 'o'=>4, 'w'=>2, 'r'=>2, 'd'=>1, 'a'=>1, 'y'=>1, 'u'=>1}))
   end
   # second spec
   # it 'explanation of what this test tests' do
