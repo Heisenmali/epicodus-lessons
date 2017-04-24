edf7c205731dca514c36431baf545d719e349163
jin camou and Niklas Long <niklas@>
2017-04-24 14:35:29 -0700

    pass integration test for returning cost of shipping

--- app.rb
+++ app.rb
@@ -4,6 +4,16 @@ require './lib/parcel'
 also_reload 'lib/**/*.rb'
 
 get '/' do
-  @greeting = "hello Sara".method_test
   erb(:index)
+end
+
+get '/result' do
+
+  length = params.fetch('length').to_i
+  width = params.fetch('width').to_i
+  weight = params.fetch('weight').to_i
+  new_parcel = Parcel.new(length, width, weight)
+  @output = new_parcel.shipping_cost?
+
+  erb(:result)
 end
--- spec/parcel_integration_spec.rb
+++ spec/parcel_integration_spec.rb
@@ -1,13 +1,16 @@
-# require 'capybara/rspec'
-# require './app'
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
-#
-# describe 'the application path', {:type => :feature} do
-#   it 'it processes the user entry and returns...' do
-#     visit '/'
-#     fill_in 'input_id', :with => 'input'
-#     click_button 'button_id'
-#     expect(page).to have_content 'expected output'
-#   end
-# end
+require 'capybara/rspec'
+require './app'
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe 'the parcel path', {:type => :feature} do
+  it 'it processes the user entry and returns shipping cost' do
+    visit '/'
+    fill_in 'length', :with => '2'
+    fill_in 'width', :with => '4'
+    fill_in 'weight', :with => '6'
+
+    click_button 'submit'
+    expect(page).to have_content '12'
+  end
+end
