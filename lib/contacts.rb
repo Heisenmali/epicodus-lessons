a42a585c68875e621da991f72cf4ed5b8ade04fa
Kat Gifford and Niklas Long <kat@>
2017-04-27 10:37:29 -0700

    add objects and objects within objects. all specs passing

---
+++ lib/contacts.rb
@@ -0,0 +1,63 @@
+class Contact
+
+  attr_accessor :name
+  attr_accessor :address
+  attr_accessor :email
+  attr_accessor :phone
+
+  def initialize(attribute)
+
+    self.name = Name.new(attribute.fetch(:first_name), attribute.fetch(:last_name))
+    self.address = Address.new(attribute.fetch(:street), attribute.fetch(:city), attribute.fetch(:state), attribute.fetch(:zip))
+    self.email = Email.new(attribute.fetch(:email))
+    self.phone = Phone.new(attribute.fetch(:cell))
+  end
+end
+
+class Name
+
+  attr_accessor :first
+  attr_accessor :last
+
+  def initialize(first_name, last_name)
+    self.first = first_name
+    self.last = last_name
+  end
+end
+
+class Address
+
+  attr_accessor :street
+  attr_accessor :city
+  attr_accessor :state
+  attr_accessor :zip
+
+  def initialize(street, city, state, zip)
+    self.street = street
+    self.city = city
+    self.state = state
+    self.zip = zip
+  end
+end
+
+class Email
+
+  attr_accessor :personal
+
+  def initialize(email)
+    self.personal = email
+  end
+end
+
+class Phone
+
+  attr_accessor :cell
+  # attr_accessor :home
+  # attr_accessor :work
+
+  def initialize(phone)
+    self.cell = phone
+    # self.home =
+
+  end
+end
