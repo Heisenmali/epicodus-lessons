1fd9198051554f23b7e4e0bf21eb681ba14107d2
nkls <niklas@long.ch>
2017-05-05 08:45:32 -0700

    add spec and method to save to projects database

--- lib/project.rb
+++ lib/project.rb
@@ -2,4 +2,9 @@ module Project
   def self.all
     DB.exec("SELECT * FROM projects;").to_a
   end
+
+  def self.save(name)
+    uuid = SecureRandom.uuid
+    DB.exec("INSERT INTO projects (id, name) VALUES ('#{uuid}', '#{name}') RETURNING id;")[0]["id"]
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -8,6 +8,14 @@ describe 'Project' do
 			expect(Project.all).to eq []
 		end
 	end
+
+	describe '.save' do
+		it 'will save a project to the Database' do
+			name = "test project"
+			project_uuid = Project.save(name)
+			expect(Project.all[0]["id"]).to eq project_uuid
+		end
+	end
 end
 
 
