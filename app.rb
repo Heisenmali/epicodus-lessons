fa783bb6939227c3e3aa5a9186db84b7dbf4817c
nkls <niklas@long.ch>
2017-05-12 10:51:16 -0700

    add routing and logic for associations

--- app.rb
+++ app.rb
@@ -9,7 +9,6 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get '/' do
   brand_id = Brand.create(name: "Paul Smith", price: "9.50").id
   store_id = Store.create(name: "Apple store", location: "portland").id
-  BrandStore.create(brand_id: brand_id, store_id: store_id)
   erb :index
 end
 
@@ -22,8 +21,11 @@ end
 
 get '/store/:id' do
   @store = Store.find(params['id'])
+  @brands = Brand.all
   erb :store
 end
+
+#Edit name and location
 
 patch '/store/:id/edit' do
   new_name = params['new-name']
@@ -32,6 +34,17 @@ patch '/store/:id/edit' do
   store.update(name: new_name, location: new_location)
   redirect back
 end
+
+#Edit associated brands
+
+post '/store/:id/edit' do
+  brand_ids = params['brand_ids']
+  store = Store.find(params['id'])
+  array_of_hashes = brand_ids.brand_to_hash(store.id)
+  BrandStore.create(array_of_hashes)
+  redirect back
+end
+
 
 delete '/store/:id/delete' do
   store = Store.find(params['id'])
@@ -48,14 +61,23 @@ end
 
 get '/brand/:id' do
   @brand = Brand.find(params['id'])
+  @stores = Store.all
   erb :brand
 end
 
-post '/brand/:id/edit' do
+patch '/brand/:id/edit' do
   new_name = params['new-name']
   new_price = params['new-price']
   brand = Brand.find(params['id'])
   brand.update(name: new_name, price: new_price)
+  redirect back
+end
+
+post '/brand/:id/edit' do
+  store_ids = params['store_ids']
+  brand = Brand.find(params['id'])
+  array_of_hashes = store_ids.store_to_hash(brand.id)
+  BrandStore.create(array_of_hashes)
   redirect back
 end
 
