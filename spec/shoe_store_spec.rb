dbacd8e647ae9a1e015c3edde179ccfe83759bfb
nkls <niklas@long.ch>
2017-05-12 09:03:34 -0700

    add unit and DB testing, all specs passing

---
+++ spec/shoe_store_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+
+describe Store do
+  it { should have_many :brands }
+end
+
+describe Brand do
+  it { should have_many :stores }
+end
+
+describe BrandStore do
+  it { should belong_to :store }
+  it { should belong_to :brand }
+end
+
+#DB TESTING
+
+describe Store do
+  it { should have_db_column :id }
+  it { should have_db_column :name }
+  it { should have_db_column :location }
+end
+
+describe Brand do
+  it { should have_db_column :id }
+  it { should have_db_column :name }
+  it { should have_db_column :price }
+end
+
+describe BrandStore do
+  it { should have_db_column :store_id }
+  it { should have_db_column :brand_id }
+end
