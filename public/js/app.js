595ecd1f8ffcb72a5fcd3af8a0133bc5be752e43
nkls <niklas@long.ch>
2017-05-13 20:52:33 -0700

    add setInterval to js

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-  $.getJSON("http://localhost:4567/exp", function(data){
-    $('div').text(data.first)
-  });
+  var async = function() {
+    $.getJSON("http://localhost:4567/exp", function(data) {
+      $('div').append(data.first)
+    });
+  }
+  setInterval(async,2000);
   document.getElementsByTagName("html")[0].style.visibility = "visible";
 });
