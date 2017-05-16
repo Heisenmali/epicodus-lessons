1b923d18e00384bf3ab99c97c17b9faee4094574
nkls <niklas@long.ch>
2017-05-15 19:33:34 -0700

    remove js, refactor app.rb

--- public/js/app.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  var async = function() {
-    $.getJSON("http://localhost:4567/exp", function(data) {
-      $('div').append(data.first)
-    });
-  }
-  setInterval(async,2000);
-  document.getElementsByTagName("html")[0].style.visibility = "visible";
-});
