22ea443ebd5514e3abef6ee1a2d8d8887eb1043c
nkls <niklas@long.ch>
2017-05-26 14:36:04 -0700

    add input and binding

--- src/components/Doctor.vue
+++ src/components/Doctor.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
+    <form>
+      <input v-bind="cond">
+    </form>
+  
     <button v-on:click="getApi">Click me!</button>
+    
     <div v-if="doctors" v-for="doctor in doctors">
       <h2> {{doctor.profile.first_name}} {{doctor.profile.last_name}} - {{doctor.profile.title}} </h2>
       <p v-for="specialty in doctor.specialties"> {{specialty.actor}} </p>
@@ -15,12 +20,13 @@ export default {
   data () {
     return {
       doctors: false,
-      apiKey: 'bf2df4b942c1fcc62beccf775d417167'
+      apiKey: 'bf2df4b942c1fcc62beccf775d417167',
+      condition: ''
     }
   },
   methods: {
-    getApi: function(medicalIssue) {
-      fetch('https://api.betterdoctor.com/2016-03-01/doctors?query='+ 'cancer'+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + this.apiKey)
+    getApi: function() {
+      fetch('https://api.betterdoctor.com/2016-03-01/doctors?query='+ this.condition +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + this.apiKey)
       .then(response => {
         return response.json();
       })
