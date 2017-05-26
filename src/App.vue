1f6812b43bb6d81e984f34d8a9a54ee525582336
nkls <niklas@long.ch>
2017-05-26 14:20:56 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <doctor></doctor>
+  </div>
+</template>
+
+<script>
+import Doctor from './components/Doctor.vue'
+export default {
+  name: 'app',
+  components: {
+    'doctor': Doctor
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Doctor.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button v-on:click="getApi">Click me!</button>
+    <div v-if="doctors" v-for="doctor in doctors">
+      <h2> {{doctor.profile.first_name}} {{doctor.profile.last_name}} - {{doctor.profile.title}} </h2>
+      <p v-for="specialty in doctor.specialties"> {{specialty.actor}} </p>
+      <p v-for="address in doctor.practices"> {{address.visit_address.street}} - {{address.visit_address.zip}} {{address.visit_address.city}} </p>
+    </div>
+  </div> 
+</template>
+
+<script>
+export default {
+  name: 'doctor',
+  data () {
+    return {
+      doctors: false,
+      apiKey: 'bf2df4b942c1fcc62beccf775d417167'
+    }
+  },
+  methods: {
+    getApi: function(medicalIssue) {
+      fetch('https://api.betterdoctor.com/2016-03-01/doctors?query='+ 'cancer'+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + this.apiKey)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.doctors = data.data;
+        console.log(this.doctors)
+      });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
