39971ac14d9700db0c22cf9ddcbbb6102c0664d7
nkls <niklas@long.ch>
2017-05-26 21:32:47 -0700

    Moves all repo_sub files into a single directory for merging back into repo_main

---
+++ tamagotchi/Ruby/contacts/app.rb
@@ -0,0 +1,44 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require './lib/contacts'
+require 'pry'
+
+also_reload('lib/**/*.rb')
+
+get('/') do
+  @output = Contact.all
+  erb(:index)
+end
+
+get('/contacts/addform') do
+  erb(:form)
+end
+
+post('/contacts/addnew') do
+
+  first_name = params.fetch("first_name")
+  last_name = params.fetch("last_name")
+  street = params.fetch("street")
+  city = params.fetch("city")
+  state = params.fetch("state")
+  zip = params.fetch("zip")
+  email = params.fetch("email")
+  cell = params.fetch("cell")
+
+  info_hash = {:first_name => first_name, :last_name => last_name, :street => street, :city => city, :state => state, :zip => zip, :email => email, :cell => cell}
+  new_contact = Contact.new(info_hash)
+  new_contact.save
+  erb(:success)
+end
+
+post('/contacts/:name/addemail') do
+  type = params.fetch("type")
+  address = params.fetch("email")
+  Contact.find(params.fetch('name')).add_email(type, Email.new(address))
+  erb(:success)
+end
+
+get('/contacts/:name') do
+  @contact = Contact.find(params.fetch('name'))
+  erb(:contact_page)
+end
---
+++ tamagotchi/Ruby/contacts/spec/contacts_spec.rb
@@ -0,0 +1,63 @@
+require "contacts"
+require "rspec"
+require "pry"
+
+describe Contact do
+
+  describe '#initialize'
+  it "initializes the Name object and returns first name" do
+    test_contact = Contact.new({:first_name => "Kat", :last_name => "Gifford", :street => "114 nw winding way", :city => "Covington", :state => "KY", :zip => "", :email => "", :cell => ""})
+    expect(test_contact.name.first).to(eq("Kat"))
+  end
+  it "initializes the Name object and returns last name" do
+    test_contact = Contact.new({:first_name => "Kat", :last_name => "Gifford", :street => "114 nw winding way", :city => "Covington", :state => "KY", :zip => "", :email => "", :cell => ""})
+    expect(test_contact.name.last).to(eq("Gifford"))
+  end
+
+  it "initializes the address object and returns the street address" do
+    test_contact = Contact.new({:first_name => "Kat", :last_name => "Gifford", :street => "114 nw winding way", :city => "Covington", :state => "KY", :zip => "", :email => "", :cell => ""})
+    expect(test_contact.address.street).to(eq("114 nw winding way"))
+  end
+  it "initializes the address object and returns the city" do
+    test_contact = Contact.new({:first_name => "Kat", :last_name => "Gifford", :street => "114 nw winding way", :city => "Covington", :state => "KY", :zip => "", :email => "", :cell => ""})
+    expect(test_contact.address.city).to(eq("Covington"))
+  end
+  it "initializes the address object and returns the state" do
+    test_contact = Contact.new({:first_name => "", :last_name => "", :street => "", :city => "Covington", :state => "KY", :zip => "", :email => "", :cell => ""})
+    expect(test_contact.address.state).to(eq("KY"))
+  end
+  it "initializes the address object and returns the zip code" do
+    test_contact = Contact.new({:first_name => "Kat", :last_name => "Gifford", :street => "114 nw winding way", :city => "Covington", :state => "KY", :zip => "41011", :email => "", :cell => ""})
+    expect(test_contact.address.zip).to(eq("41011"))
+  end
+
+  it "initializes the email object and returns the email" do
+    test_contact = Contact.new({:first_name => "Kat", :last_name => "", :street => "", :city => "", :state => "", :zip => "", :email => "piKatso@me.com", :cell => ""})
+    expect(test_contact.email.personal).to(eq("piKatso@me.com"))
+  end
+
+  it "initializes the phone object and returns the cell" do
+    test_contact = Contact.new({:first_name => "Kat", :last_name => "", :street => "", :city => "", :state => "", :zip => "", :email => "", :cell => "5036082203"})
+    expect(test_contact.phone.cell).to(eq("5036082203"))
+  end
+
+  describe "#save"
+  it "saves contact to contacts array" do
+    test_contact = Contact.new({:first_name => "Kat", :last_name => "", :street => "", :city => "", :state => "", :zip => "", :email => "", :cell => ""})
+    test_contact.save
+    expect(Contact.all).to(eq([test_contact]))
+  end
+
+  describe "#clear"
+  it "clears contact to contacts array" do
+    test_contact = Contact.new({:first_name => "Kat", :last_name => "", :street => "", :city => "", :state => "", :zip => "", :email => "", :cell => ""})
+    test_contact.save
+    expect(Contact.clear).to(eq([]))
+  end
+
+  it "" do
+    test_contact = Contact.new({:first_name => "Kat", :last_name => "", :street => "", :city => "", :state => "", :zip => "", :email => "", :cell => ""})
+    expect(Contact.all).to(eq([]))
+  end
+
+end
