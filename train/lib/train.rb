c8dbe0d07edd73bcf31b673b48bb2fe224a5e986
nkls <niklas@long.ch>
2017-05-26 17:32:43 -0700

    Moves all repo_sub files into a single directory for merging back into repo_main

---
+++ train/lib/train.rb
@@ -0,0 +1,34 @@
+require 'securerandom'
+
+module Train
+  def self.all
+    DB.exec("SELECT * FROM trains;").to_a
+  end
+
+  def self.save(name)
+    uuid = SecureRandom.uuid
+    DB.exec("INSERT INTO trains (id, name) VALUES ('#{uuid}', '#{name}') RETURNING id;")[0]["id"]
+  end
+
+  def self.find(uuid)
+    DB.exec("SELECT * FROM trains where id = '#{uuid}';")[0]
+  end
+
+  def self.edit(name, uuid)
+    DB.exec("UPDATE trains SET name = '#{name}' WHERE id = '#{uuid}';")
+  end
+
+  def self.delete(uuid)
+    DB.exec("DELETE FROM trains WHERE id = '#{uuid}';")
+  end
+
+  def self.train_cities(uuid)
+    DB.exec("SELECT city_id, stop_time FROM trains_cities WHERE train_id = '#{uuid}';").to_a
+    end
+
+  def self.add_train_cities(train_uuid, city_uuid_a, stop_time_a)
+    city_uuid_a.each_with_index do |city_uuid, index|
+      DB.exec("INSERT INTO trains_cities (train_id, city_id, stop_time) VALUES ('#{train_uuid}', '#{city_uuid}', '#{stop_time_a[index]}');")
+    end
+  end
+end
---
+++ train/spec/int_helper_spec.rb
@@ -0,0 +1,31 @@
+require "capybara/rspec"
+require "./app"
+require 'securerandom'
+
+
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+#
+module DBI
+  def self.load_data #load data into database
+    train_name = "The Burf Express"
+    Train.save(train_name)
+    city_name = "Portland"
+    City.save(city_name)
+  end
+end
+
+RSpec.configure do |config|
+  config.before(:each) do
+    extend DBI
+    DBI.load_data
+  end
+end
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM trains *;")
+    DB.exec("DELETE FROM cities *;")
+    DB.exec("DELETE FROM trains_cities *;")
+  end
+end
