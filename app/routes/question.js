7e9f77c4ad2a3b6bd380029eba29a27e63474c54
nkls <niklas@long.ch>
2017-06-02 11:46:56 -0700

    add answers

--- app/routes/question.js
+++ app/routes/question.js
@@ -9,6 +9,14 @@ export default Ember.Route.extend({
         }
       });
       question.save();
+    },
+    addAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function() {
+        return question.save();
+      });
     }
   }
 });
