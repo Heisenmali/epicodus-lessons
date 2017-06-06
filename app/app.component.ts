30f783e2e0bf9e95d13e01ed0f9e1237a09135b2
Niklas Long and Sara Schultz <sara@>
2017-06-06 09:25:21 -0700

    add working recipe component

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Bon matin squelette!</h1>
+  <div *ngFor="let recipe of recipes">
+    <h2>{{recipe.title}}</h2>
+    <ul>
+      <li *ngFor="let ingredient of recipe.ingredients">{{ingredient}}</li>
+    </ul>
+    <ol>
+      <li *ngFor="let direction of recipe.directions">{{direction}}</li>
+    </ol>
+  </div>
   `
 })
 
 export class AppComponent {
+  recipes: Recipe[] = [
+    new Recipe('Chocolate brownie', ['chocolate', 'chocolate', 'more chocolate'], ['aquire chocolate', 'do the thang', 'eat leftover chocolate'])
+  ];
+}
 
+export class Recipe {
+  constructor (public title: string, public ingredients: string[], public directions: string[]) { }
 }
