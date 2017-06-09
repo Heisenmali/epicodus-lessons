2cd3b8d4645861133db04589932721e988f264a8
nkls <niklas@long.ch>
2017-06-09 09:09:37 -0700

    add component to list all animals and their information

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Animal } from './animal.model';
 
 @Component({
   selector: 'app-root',
   template: `
-    <list-animal></list-animal>
+    <list-animal [animals]="animals"></list-animal>
   `
 })
 
 export class AppComponent {
-
+  animals: Animal[] = [
+    new Animal('Arctic Fox', 'Moon', 2, 'Carnivore', 'Northern Trail', 3, 'Female', 'Chocolate', 'Olives'),
+    new Animal('Black Leopard', 'Goeff', 4, 'Carnivore', 'Southern Trail', 1, 'Male', 'Chicken', 'Daylight'),
+    new Animal('Black Tailed Deer', 'Tinkerbell', 7, 'Herbivore', 'Northern Trail', 2, 'Female', 'Leaves', 'Loud Noises')
+  ];
 }
+
+
--- app/list-animal.component.ts
+++ app/list-animal.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Animal } from './animal.model';
 
 @Component({
   selector: 'list-animal',
   template: `
-    <h1>Animal list</h1>
+  <div *ngFor="let animal of animals">
+    <h3>{{animal.name}} - {{animal.species}}</h3>
+    <p>Age: {{animal.age}}</p>
+    <p>Diet: {{animal.diet}}</p>
+    <p>Location: {{animal.location}}</p>
+    <p>Caretakers: {{animal.caretakers}}</p>
+    <p>Sex: {{animal.sex}}</p>
+    <p>Likes: {{animal.likes}}</p>
+    <p>Dislikes: {{animal.dislikes}}</p>
+  </div>
   `
 })
 
+
 export class ListAnimalComponent {
-  
+@Input() animals: Animal[];
+
+
+
 }
