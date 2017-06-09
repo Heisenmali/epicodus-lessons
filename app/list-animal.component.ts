50516fac3ed8788659245144739a42c6ddef619d
nkls <niklas@long.ch>
2017-06-09 08:36:07 -0700

    link animal list component

---
+++ app/list-animal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'list-animal',
+  template: `
+    <h1>Animal list</h1>
+  `
+})
+
+export class ListAnimalComponent {
+  
+}
