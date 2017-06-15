a83cb70973e1153625126c27679541f27cae2731
Kat Gifford and Niklas Long <niklas@>
2017-06-15 11:21:21 -0700

    add compareObjects() and addMines()

---
+++ app.js
@@ -0,0 +1,84 @@
+var grid =
+
+[
+  ["s", "s", "s", "s", "s", "s", "s", "s"],
+  ["s", "s", "s", "s", "s", "s", "s", "s"],
+  ["s", "s", "s", "s", "s", "s", "s", "s"],
+  ["s", "s", "s", "s", "s", "s", "s", "s"],
+  ["s", "s", "s", "s", "s", "s", "s", "s"],
+  ["s", "s", "s", "s", "s", "s", "s", "s"],
+  ["s", "s", "s", "s", "s", "s", "s", "s"],
+  ["s", "s", "s", "s", "s", "s", "s", "s"]
+];
+
+//grid[y][x]
+
+var compareObjects = function(one, two) {
+  if (one === undefined) {
+    return false
+  }
+
+  var x_values = false;
+  var y_values = false;
+
+  if (one.x === two.x)
+    x_values = true;
+
+  if (one.y === two.y)
+    y_values = true;
+
+  if (x_values === true && y_values === true) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+var addMines = function() {
+//  1. Select a random square from the grid record its coordinates
+//  2. Assign it as a mine
+//  3. Keep count of mine number
+//  4. select  new random square check if it's already been selected. If so select a new one. If not go to step 2
+
+  var mines = [];
+
+  while (mines.length < 10) {
+    var selectedMine = {x: 0, y: 0}
+    var duplicates = false;
+
+    selectedMine.x = Math.floor(Math.random() * 8); // 0 <= x <= 7
+    selectedMine.y = Math.floor(Math.random() * 8);
+    // console.log(selectedMine)
+
+    if (mines.length === 0 ) {
+      mines.push(selectedMine);
+      continue;
+    }
+
+    for (var i in mines) {
+      if (!compareObjects(mines[i], selectedMine)) {
+        duplicates = false;
+      } else {
+        duplicates = true;
+      }
+    }
+    if (!duplicates) {
+      mines.push(selectedMine);
+    }
+  }
+
+  console.log(mines)
+}
+
+
+
+//running the app
+var run = function() {
+  // console.log(grid);
+}
+
+window.onload = function() {
+  addMines();
+  run();
+}
