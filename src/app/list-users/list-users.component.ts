eed9399170d51cce92b3c6fc098f5a4534208736
nkls <niklas@long.ch>
2017-06-16 09:00:40 -0700

    add database service, display firebase data

--- src/app/list-users/list-users.component.ts
+++ src/app/list-users/list-users.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user.model';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-list-users',
   templateUrl: './list-users.component.html',
-  styleUrls: ['./list-users.component.css']
+  styleUrls: ['./list-users.component.css'],
+  providers: [UserService]
 })
 export class ListUsersComponent implements OnInit {
-
-  constructor() { }
+  users: any[];
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.userService.getUsers().subscribe(data => {
+      this.users = data;
+    })
   }
 
 }
---
+++ src/app/user.model.ts
@@ -0,0 +1,3 @@
+export class User {
+  constructor (public name: string, public age: number, public bio: string, public goal: string) {}
+}
