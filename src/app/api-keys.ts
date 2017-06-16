14959ddcd913fdd32f73f6182a186a8173ffd50a
Niklas Long <niklaslong@users.noreply.github.com>
2017-06-16 11:16:29 -0700

    Delete api-keys.ts

--- src/app/api-keys.ts
+++
@@ -1,8 +0,0 @@
-export var masterFirebaseConfig = {
-    apiKey: "AIzaSyCPt-fscGpP2a89A3TlAVfOqWo2Xh6nbdg",
-    authDomain: "club-bf5b8.firebaseapp.com",
-    databaseURL: "https://club-bf5b8.firebaseio.com",
-    projectId: "club-bf5b8",
-    storageBucket: "club-bf5b8.appspot.com",
-    messagingSenderId: "93727865977"
-  };
