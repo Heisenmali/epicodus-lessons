ae8d1827c045dfa2ac665e2024b3b3b1f52c1c8d
Niklas Long and Tanner Eustice <niklas@>
2017-06-26 11:25:38 -0700

    add bootstrap

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,31 +1,29 @@
 class ListsController < ApplicationController
   def index
     @lists = List.all
-    render :index
   end
 
   def show
     @list = List.find(params[:id])
-    render :show
   end
 
-  def new 
+  def new
     @list = List.new
-    render :new
   end
+
 
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to lists_path
-    else 
+      flash[:notice] = "List successfully added!"
+      redirect_to list_path(@list)
+    else
       render :new
     end
   end
 
   def edit
     @list = List.find(params[:id])
-    render :edit
   end
 
   def update
@@ -43,7 +41,7 @@ class ListsController < ApplicationController
     redirect_to lists_path
   end
 
-  private 
+  private
     def list_params
       params.require(:list).permit(:name)
     end
