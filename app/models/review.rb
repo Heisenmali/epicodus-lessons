a52c44a54aa55437abec9a16b8a7b6704a9bbf1a
nkls <niklas@long.ch>
2017-06-30 12:39:34 -0700

    add rating and content length validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,7 @@ class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
 
-  validates :rating, :content, presence: true
+  validates :rating, presence: true, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 1 }
+  validates :content, presence: true, length: { in: 50..250 }
 
 end
