cf0b9ac1e220e2576017f3a341b64b8273867547
nkls <niklas@long.ch>
2017-06-30 11:38:41 -0700

    add new#review and create#review controller actions

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,22 @@
+class ReviewsController < ApplicationController
+
+def new
+    @product = Product.find(params[:product_id])
+    @review = Review.new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.create(review_params)
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+private
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+end
