b4ad5f0f43afa5cf710e119aa19a4efeb8f6b94e
nkls <niklas@long.ch>
2017-06-30 09:52:25 -0700

    seed database with Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,15 @@
 Product.destroy_all
+User.destroy_all
+
+user = User.create(name: "nkls")
+
+50.times do |i|
+  product = Product.create(name: Faker::Ancient.primordial, cost:Faker::Number.digit , country: Faker::Address.country)
+
+  5.times do |j|
+    review = Review.create(rating: Faker::Number.between(1, 5), content: Faker::Hipster.sentence)
+
+    user.reviews << review
+    product.reviews << review
+  end
+end
