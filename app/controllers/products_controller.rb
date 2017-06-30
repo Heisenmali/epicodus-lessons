b3ac7cf63428c145b1b93b7a6c0eaabb12525d39
nkls <niklas@long.ch>
2017-06-30 10:09:09 -0700

    add create for Product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,24 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      flash[:notice] = "product was saved"
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+
+private
+  def product_params
+    params.require(:product).permit(:name, :cost, :country)
+  end
 end
