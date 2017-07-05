e207a810f2e48214de409863b97689b169b1c271
Niklas Long and Tanner Eustice <niklas@>
2017-07-05 10:00:02 -0700

    initial commit, setup devise

---
+++ app/controllers/landing_controller.rb
@@ -0,0 +1,8 @@
+class LandingController < ApplicationController
+  def index
+    if current_user
+      @user = current_user
+      @photos = current_user.photos
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'landing#index'
+  devise_for :users
+  resources :photos
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
