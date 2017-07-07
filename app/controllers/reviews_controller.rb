9c4641dd32bd3d247c5e8acc817cca16632627f0
nkls <niklas@long.ch>
2017-07-07 11:43:20 -0700

    add integration specs for reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,14 +6,14 @@ class ReviewsController < ApplicationController
   end
 
   def create 
-    product = Product.find_by(id: params[:product_id])
-    review = Review.create(review_params)
+    @product = Product.find_by(id: params[:product_id])
+    @review = Review.create(review_params)
     user = current_user
-    user.reviews << review
-    product.reviews << review
+    user.reviews << @review
+    @product.reviews << @review
 
-    if review.save
-      redirect_to product_path(product)
+    if @review.save
+      redirect_to product_path(@product)
     else
       render :new
     end
