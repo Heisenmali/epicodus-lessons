e9c6ce465683b30cd9f005e0bf9f9b7d370ebda7
nkls <niklas@long.ch>
2017-07-07 09:16:33 -0700

    add devise authentication for rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+
+  root to: 'products#index'
   
   resources :products do
     resources :reviews
