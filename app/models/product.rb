b25ce8502573f9ef2e955062545329371fafe643
nkls <niklas@long.ch>
2017-07-07 12:07:00 -0700

    implement paperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
   has_many :reviews
   has_many :users, through: :reviews
+
+  has_attached_file :image
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
