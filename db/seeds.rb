b69c75ad934952a1eefdf9d9bd028907cdc8a752
nkls <niklas@long.ch>
2017-07-07 14:15:10 -0700

    modify seed data to include images

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,12 @@ Product.destroy_all
 Review.destroy_all
 User.destroy_all
 
-user = User.create(email: 'rough@start', password: 'donkez', password_confirmation: "donkez")
+user = User.create(email: 'rough@start', password: 'donkez', password_confirmation: "donkez", admin: true)
 
-20.times do |i|
-  product = Product.create(name: Faker::Hipster.word, description: Faker::Hipster.paragraph(3))
+10.times do |i|
+  product = Product.create(name: Faker::Hipster.word, description: Faker::Hipster.paragraph(3), price: Faker::Number.decimal(2), image: File.open(ENV['HOME'] + "/desktop/music_store/public/seed_images/#{i}.jpg"))
 
-  5.times do |i|
+  5.times do |j|
     review = Review.create(content: Faker::Hipster.paragraph(2))
 
     product.reviews << review
