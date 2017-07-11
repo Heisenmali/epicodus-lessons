e906770d71f76c11a19b9ad5259089cfadde9209
nkls <niklas@long.ch>
2017-07-11 08:24:59 -0700

    remove order item specs

--- spec/controllers/order_items_controller_spec.rb
+++ spec/controllers/order_items_controller_spec.rb
@@ -1,11 +1,2 @@
-require 'rails_helper'
 
-describe OrderItemsController do
-  before :each do
-    @user = FactoryGirl.create(:user)
-    login_as(user, :scope => :user)
-  end
 
-  describe '#index' do
-  end
-end
