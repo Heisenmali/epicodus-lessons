6f2038278fb08c764427630fabb4f14a0c3dd0dd
nkls <niklas@long.ch>
2017-07-20 15:59:49 -0700

    update seed file to include image component

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,13 @@ Component.destroy_all
 
 post = Post.create(author: 'nkls')
 
-post_heading = Component.create(component_type: 'post_heading', content: 'hello world, this is the post heading component')
+post_heading = Component.create(component_type: 'post-heading', content: 'hello world, this is the post heading component')
 
-post_paragraph = Component.create(component_type: 'post_paragraph', content: 'hello world, this is the post paragraph component')
+post_paragraph = Component.create(component_type: 'post-paragraph', content: 'hello world, this is the post paragraph component')
 
+post_image = Component.create(component_type: 'post-image', content: 'https://s3.eu-central-1.amazonaws.com/bynk-blog/bynk-blog/identicon.png')
 post.components << post_heading
 post.components << post_paragraph
+post.components << post_image
+
+puts 'DONE RAN THE SEED'
