145117a8e1b4c76bd20619372718f4ce2b640c3a
nkls <niklas@long.ch>
2017-07-21 12:31:01 -0700

    problems with testing post request

--- spec/requests/parks_spec.rb
+++ spec/requests/parks_spec.rb
@@ -5,13 +5,41 @@ describe "Messages API" do
     FactoryGirl.create_list(:park, 10)
 
     get '/parks'
-
     json = JSON.parse(response.body)
 
     # test for the 200 status-code
     expect(response).to be_success
 
     # check to make sure the right amount of messages are returned
-    expect(json['parks'].length).to eq(10)
+    expect(json['data'].length).to eq(10)
+  end
+
+  it 'gets a specific park' do
+    park = FactoryGirl.create_list(:park, 10).first
+
+    get '/parks/' + park.id.to_s
+    json = JSON.parse(response.body)
+   
+    # test for the 200 status-code
+    expect(response).to be_success
+
+    # check to make sure the right amount of messages are returned
+    expect(json.length).to eq(1)
+    expect(json['data']['id']).to eq(park.id.to_s)
+  end
+
+  it 'creates a park' do
+    park = FactoryGirl.create_list(:park, 10).first
+    headers = { "CONTENT_TYPE" => "application/vnd.api+json", "ACCEPT" => "application/vnd.api+json"}
+    post "/parks", params: {"data" => {"type" => "parks", "attributes" => {"name" => "To TDD or Not"}}}, headers: headers
+    json = JSON.parse(response.body)
+    byebug
+   
+    # test for the 200 status-code
+    expect(response).to be_success
+
+    # check to make sure the right amount of messages are returned
+    expect(json.length).to eq(1)
+    expect(json['data']['id']).to eq(park.id.to_s)
   end
 end
