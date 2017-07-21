4be79a10de6aa6d060f489e9a4c81c74e4e0da31
nkls <niklas@long.ch>
2017-07-21 14:13:24 -0700

    add update controller action

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -10,10 +10,24 @@ class ParksController < ApplicationController
     json_response(park)
   end
 
+  def update
+    byebug
+    park = Park.find(params[:id])
+    if park.update!(park_params)
+      json_response(message: "song was updated")
+    end
+  end
+
   def destroy
     park = Park.find(params[:id])
     if park.destroy!
       json_response(message: "song was deleted")
     end
+  end
+
+  private
+
+  def park_params
+    params.permit(:name)
   end
 end
