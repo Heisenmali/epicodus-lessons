e2f68efc9026b57e359317ccc46c4820a17e48e6
nkls <niklas@long.ch>
2017-07-21 21:08:19 -0700

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ post_heading = Component.create(component_type: 'post-heading', content: 'hello 
 
 post_paragraph = Component.create(component_type: 'post-paragraph', content: 'hello world, this is the post paragraph component')
 
-post_image = Component.create(component_type: 'post-image', content: 'https://s3.eu-central-1.amazonaws.com/bynk-blog/bynk-blog/identicon.png')
+post_image = Component.create(component_type: 'post-image', content: 'https://s3.eu-central-1.amazonaws.com/bynk-blog/bynk-blog/thing.jpeg')
 post.components << post_heading
 post.components << post_paragraph
 post.components << post_image
